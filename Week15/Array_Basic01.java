7cc2050903ef3bf1f7a50971fd4ebe7c52d3e81d
aoaae <s.suksawat@webmail.npru.ac.th>
2021-10-19 16:12:48 +0700

    Week15

---
+++ Week15/Array_Basic01.java
@@ -0,0 +1,23 @@
+package Week15;
+
+public class Array_Basic01 {
+    public static void main(String[] args) {
+       long [] num = new long [200];   // แบบ 2
+       long [] num = {1,2,3,4,5,6,7,8,9,2,2,2,3,3,3,34,4};  // แบบ 1
+
+       int [] dice = {1,2,3,4,5,6};
+       int [] dice = new int [6];
+
+       float [] avgGrade = new float [451];
+       float [] avgGrade = {1.30,2.02,3.22,4.00,1.05,2.06,.........} //กรอกให้ครบ 451 ตัว
+
+       char [] grade = new char [369];
+       char [] grade = {'A','B','C','A','B','B','C','E'........};//กรอกให้ครบ 369  ตัว
+
+
+
+
+
+
+    }
+}
